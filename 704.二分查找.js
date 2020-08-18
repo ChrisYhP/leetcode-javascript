f0912b2438a3d8ff6ed59b09957d01383cba12ef
严厚谱 <yanhoup@4px.com>
2020-08-18 16:30:01 +0800

    二分查找

---
+++ 704.二分查找.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=javascript
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  var left = 0;
+  var right = nums.length - 1;
+  while (left <= right) {
+    var mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    }
+  }
+  return - 1;
+};
+console.log(search([-1, 0, 3, 5, 9], 0))
+
+// @lc code=end
+

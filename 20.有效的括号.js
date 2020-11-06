ffe55aa015f1804be30f9ca5ac080755ef86c346
严厚谱 <yanhoup@4px.com>
2020-11-06 15:13:07 +0800

    有效括号

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -9,8 +9,24 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-
+var isValid = function (s) {
+  var map = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+  var arr = s.split("");
+  console.log(arr);
+  var stack = [];
+  for (let i = 0; i < arr.length; i++) {
+    var a = arr[i];
+    if (map[stack[stack.length - 1]] === a) {
+      stack.pop();
+    } else {
+      stack.push(arr[i]);
+    }
+  }
+  return stack.length === 0;
 };
+console.log(isValid("()[]{}"));
 // @lc code=end
-
